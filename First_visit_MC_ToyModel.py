552d2abbb4d73bee8b5663c4c69580d6d65c9a03
AlpoGIT <47441844+AlpoGIT@users.noreply.github.com>
2019-02-17 23:16:02 -1000

    Update First_visit_MC_ToyModel.py

--- First_visit_MC_ToyModel.py
+++ First_visit_MC_ToyModel.py
@@ -1,3 +1,10 @@
+'''
+# First-Visit-MC
+First-visit MC on toy model from "Reinforcement Learning with Replacing Eligibility Traces"(Sutton 96): two states Markov chain
+On each step, the chain either stays in S = 0 with probability p = 0.75, or goes on to terminate in T = 1 with probability 1-p.
+We wish to estimate the expected number of steps before termination when starting in S.
+'''
+
 import numpy as np
 from collections import defaultdict
 
